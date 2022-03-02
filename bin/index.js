26877ff14fc089fa8714bd7a5383d0300896746c
CDM <cdmgtri@users.noreply.github.com>
2022-03-02 13:16:08 -0500

    Added HLVA 1.0 and 5.0

--- bin/index.js
+++ bin/index.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 
 // Parse rule and definition info from NIEM specifications and save results to /output/
-require("../src/index").parse();
+require("../src/index").parse("output", true);
